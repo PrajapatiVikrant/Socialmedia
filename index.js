92a320b6ec27bda75b40e6ba0ff8526812dfe782
PrajapatiVikrant <146848720+PrajapatiVikrant@users.noreply.github.com>
2023-10-25 13:40:45 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,20 +3,6 @@ require("./Config/mydb");
 const cors = require("cors");
 const app = express();
 app.use(cors());
-
-//all middleware
-app.get('/getall',async(req,res)=>{
-    console.log('hello')
-    let data = await userModel.find({});
-    console.log(data)
-    res.send(data);
-})
-app.delete('/deleteall',async(req,res)=>{
-    console.log('hellodelete')
-    let data = await userModel.deleteMany({email:'ajay@gmail.com'})
-    console.log(data)
-    res.send(data);
-})
 app.use('/Socialmedia',require("./Route"))
 
 
